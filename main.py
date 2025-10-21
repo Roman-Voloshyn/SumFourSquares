4adc0372a4a41c9ba225391378a7c49aece66fda
Roman <136504343+Roman-Voloshyn@users.noreply.github.com>
2025-10-21 21:04:30 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,134 @@
+import gmpy2
+
+
+RABIN_EXCEPTIONS = {
+    5: (2, 1, 0, 0),
+    10: (3, 1, 0, 0),
+    13: (3, 2, 0, 0),
+    34: (5, 3, 0, 0),
+    37: (6, 1, 0, 0),
+    58: (7, 3, 0, 0),
+    61: (6, 5, 0, 0),
+    85: (9, 2, 0, 0),
+    130: (11, 3, 0, 0),
+    214: (14, 3, 3, 0),
+    226: (15, 1, 0, 0),
+    370: (19, 3, 0, 0),
+    526: (21, 9, 2, 0),
+    706: (25, 9, 0, 0),
+    730: (27, 1, 0, 0),
+    829: (27, 10, 0, 0),
+    1414: (33, 18, 1, 0),
+    1549: (35, 18, 0, 0),
+    1906: (41, 15, 0, 0),
+    2986: (45, 31, 0, 0),
+    7549: (85, 18, 0, 0),
+    9634: (97, 15, 0, 0),
+}
+
+
+class RNG:
+
+    A = gmpy2.mpz(1103515245)
+    B = gmpy2.mpz(12345)
+    M = gmpy2.mpz(0x7fffffff)
+
+    def __init__(self):
+        self.seed = gmpy2.mpz(1)
+
+
+    def next_31_bit(self):
+        self.seed = (self.A * self.seed + self.B) % self.M
+        return self.seed
+
+
+    def next_range(self, bound):
+        width = bound.bit_length()
+        result = gmpy2.mpz(0)
+        for pos in range(0, width, 31):
+            result = (result << 31) | self.next_31_bit()
+        return result % bound
+
+
+def get_remainders(a, b, bound):
+    bound_sqrt = gmpy2.isqrt(bound)
+    result = []
+    while b != 0:
+        r = a % b
+        if r <= bound_sqrt:
+            result.append(r)
+        a, b = b, r
+    return result
+
+
+def two_squares(p):
+    if p == 1:
+        return [1, 0, 0, 0]
+    elif p == 2:
+        return [1, 1, 0, 0]
+
+    rng = RNG()
+    while True:
+        q = rng.next_range(p)
+        while gmpy2.powmod(q, (p - 1) // 2, p) != p - 1:
+            q = rng.next_range(p)
+        x = gmpy2.powmod(q, (p - 1) // 4, p)
+        rems = get_remainders(p, x, p)
+        if not len(rems) <= 2:
+            break
+
+    return [rems[0], rems[1], 0, 0]
+
+def three_squares(n):
+    n = gmpy2.mpz(n)
+    if n in RABIN_EXCEPTIONS:
+        return RABIN_EXCEPTIONS[n]
+    elif n % 4 == 0:
+        sub = three_squares(n // 4)
+        return [sub[0] * 2, sub[1] * 2, sub[2] * 2, sub[3] * 2]
+    elif n % 8 == 7:
+        return [0, 0 , 0, 0]
+    elif n % 8 == 3:
+        rng = RNG()
+        while True:
+            x = rng.next_range(gmpy2.isqrt(n) + 1)
+            p = (n - x * x) // 2
+            if not((n - x * x) % 2 != 0 or not (gmpy2.is_prime(p) or p == 1)):
+                break
+        two = two_squares(p)
+        return [x, two[0] + two[1], abs(two[0] - two[1]), 0]
+    elif gmpy2.is_square(n):
+        return [gmpy2.isqrt(n), 0, 0, 0]
+    else:
+        rng = RNG()
+        while True:
+            x = rng.next_range(gmpy2.isqrt(n) + 1)
+            p = (n - x * x)
+            if gmpy2.is_prime(p):
+                break
+        two = two_squares(p)
+        return [x, two[0], two[1], 0]
+
+
+def four_squares(n: int):
+    n = gmpy2.mpz(n)
+
+    if n == 1:
+        return [1, 0, 0, 0]
+    elif n == 2:
+        return [1, 1, 0, 0]
+    elif n == 3:
+        return [1, 1, 1, 0]
+
+    if n % 4 == 0:
+        sub = four_squares(n // 4)
+        return [*map(int, [sub[0] * 2, sub[1] * 2, sub[2] * 2, sub[3] * 2])]
+
+    if gmpy2.is_square(n):
+        return [*map(int, [gmpy2.isqrt(n), 0, 0, 0])]
+
+    if n % 8 == 7:
+        sub = three_squares(n - 4)
+        return [*map(int, [sub[0], sub[1], sub[2], 2])]
+
+    return [*map(int, three_squares(n))]
